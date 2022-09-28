673500c1d7f3a5c96f08038e84670f4771384f2a
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-09-28 15:59:01 +1300

    do day02 part2

--- src/day02.rs
+++ src/day02.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 use cached::proc_macro::cached;
 use itertools::iproduct;
@@ -14,6 +15,20 @@ const UP: Position = Position::new(0, -1);
 const LEFT: Position = Position::new(-1, 0);
 const RIGHT: Position = Position::new(1, 0);
 const DOWN: Position = Position::new(0, 1);
+
+#[cached]
+fn act_grid() -> HashMap<Position, char> {
+    let mut vals = (1..).map(|x| char::from_digit(x, 16).unwrap().to_ascii_uppercase());
+    let mut base = HashMap::new();
+    for a in (0..5) {
+        let k = 2_i32 - (2_i32 - a).abs();
+        for b in ((2 - k)..=(2 + k)) {
+            let p = Position::new(b, a);
+            base.insert(p, vals.next().unwrap());
+        }
+    }
+    base
+}
 
 fn make_grid(n: u32) -> Grid {
     Grid::from_iter(iproduct!(0..n, 0..n).map(|(a, b)| {
@@ -39,8 +54,7 @@ fn conv(c: char) -> Position {
     }
 }
 
-fn move_(mut position: Position, moves:&[Position]) -> Position {
-    let g = grid();
+fn move_<T>(g: &HashMap<Position, T>, mut position: Position, moves: &[Position]) -> Position {
     for m in moves {
         let p = position + m;
         if g.contains_key(&p) {
@@ -60,10 +74,11 @@ fn parse(base: &str) -> Vec<Vec<Position>> {
 
 pub fn part1(base: &str) -> i32 {
     let g = grid();
+
     let data = parse(base);
     data.iter()
         .scan(Position::new(1, 1), |state, x| {
-            *state = move_(*state, x);
+            *state = move_(&g, *state, x);
             g.get(state)
         })
         .map(|d| char::from_digit(*d as u32, 10).unwrap())
@@ -72,8 +87,14 @@ pub fn part1(base: &str) -> i32 {
         .unwrap()
 }
 
-pub fn part2(base: &str) -> i32 {
-    //let data = parse(base);
+pub fn part2(base: &str) -> String {
+    let g = act_grid();
 
-    todo!();
+    let data = parse(base);
+    data.iter()
+        .scan(Position::new(0, 2), |state, x| {
+            *state = move_(&g, *state, x);
+            g.get(state)
+        })
+        .collect()
 }
