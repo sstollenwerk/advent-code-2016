8cd00076ef50dc966078338747ddca3c960a7d17
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-01 07:41:51 +1300

    Do day13 with closures instead of hardcoded globals

--- src/day13_try_1.rs
+++ src/day13_try_1.rs
@@ -2,7 +2,7 @@ use num_complex::Complex;
 
 use pathfinding::directed::astar::astar;
 
-use pathfinding::directed::dijkstra::dijkstra;
+use pathfinding::directed::dijkstra::dijkstra_partial;
 
 type Num = i32;
 
@@ -11,6 +11,8 @@ type Position = Complex<Num>;
 type Edge = (Position, u32);
 
 const DESTINATION: Position = Position::new(31, 39);
+
+const TOCHECK: Num = 50;
 
 fn is_even(n: u32) -> bool {
     (n % 2) == 0
@@ -33,11 +35,19 @@ fn adj(p: &Position) -> Vec<Position> {
 }
 
 fn neighbours1(p: &Position, valid: &dyn Fn(&Position) -> bool) -> Vec<Edge> {
-    adj(p).into_iter().filter(|c| valid(c) ).map(|c| (c, 1)).collect()
+    adj(p)
+        .into_iter()
+        .filter(|c| valid(c))
+        .map(|c| (c, 1))
+        .collect()
 }
 
 fn heuristic(p: &Position, dest: &Position) -> u32 {
     (dest - p).l1_norm() as u32
+}
+
+fn stop(p: &Position, start: &Position, to_check: Num) -> bool {
+    (start - p).l1_norm() > to_check
 }
 
 fn parse(s: &str) -> Num {
@@ -46,19 +56,25 @@ fn parse(s: &str) -> Num {
 
 pub fn part1(s: &str) -> u32 {
     let r = parse(s);
-    println!("{:?}", &r);
     let start = Position::new(1, 1);
-    let dest = Position::new(7, 4);
-    //  let dest = DESTINATION;
-    let valid = |&p| is_valid(&r, &p, &dest);
-    let neighbours = |&p| neighbours1(&p, &valid);
-    let heur = |&p| heuristic(&p, &dest);
-    let success = |&p| heur(&p) == 0;
-    let (path, cost) = astar(&start, neighbours, heur, success).unwrap();
-    println!("{:?}", &path);
+    let dest = DESTINATION;
+    let valid = |&p: &Position| is_valid(&r, &(p.clone()), &dest);
+    let neighbours = |&p: &Position| neighbours1(&(p.clone()), &valid);
+    let heur = |&p: &Position| heuristic(&(p.clone()), &dest);
+    let success = |&p: &Position| heur(&(p.clone())) == 0;
+    let (_, cost) = astar(&start, neighbours, heur, success).unwrap();
     cost
 }
 
-pub fn part2(s: &str) -> u32 {
-    todo!();
+pub fn part2(s: &str) -> usize {
+    let r = parse(s);
+    let start = Position::new(1, 1);
+    let dest = DESTINATION;
+    let valid = |&p: &Position| is_valid(&r, &(p.clone()), &dest);
+    let neighbours = |&p: &Position| neighbours1(&(p.clone()), &valid);
+    let quit = |&p: &Position| stop(&(p.clone()), &start, TOCHECK);
+
+    let (paths, _) = dijkstra_partial(&start, neighbours, quit);
+    let costs = paths.values().map(|p| p.1);
+    costs.filter(|c| c <= &(TOCHECK as u32)).count() + 1
 }
