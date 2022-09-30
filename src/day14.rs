9266deaf209b3320b02fc8f55941a2782dbb13cd
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-01 10:47:23 +1300

    Do day14part2 In addition to parallelism seems I had to convert to String each time. Also complicated things

--- src/day14.rs
+++ src/day14.rs
@@ -1,20 +1,42 @@
 use md5::{Digest, Md5};
-type Num = u32;
-type Hashed = Vec<u8>;
 
-fn as_u4(vals: &[u8]) -> Hashed {
+use rayon::prelude::*;
+
+type Num = u32;
+type Hashed = Vec<char>;
+
+fn as_u4(vals: &[u8]) -> Vec<u8> {
     vals.iter().flat_map(|k| [k / 16, k % 16]).collect()
 }
 
-fn find_hash(s: &str) -> Hashed {
-    let mut hasher = Md5::new();
-    hasher.update(s);
-
-    as_u4(&hasher.finalize())
+fn as_s(vals: &[u8]) -> Hashed {
+    let r = as_u4(vals);
+    r.into_iter()
+        .map(|n| char::from_digit(n as u32, 16).unwrap())
+        .collect()
 }
 
-fn hasher(salt: &str, n: Num) -> Hashed {
-    find_hash(&(salt.to_owned() + &(n.to_string())))
+fn find_hash(r_: &Hashed, steps: Num) -> Hashed {
+    let mut r = r_.to_vec();
+    for _ in (0..steps) {
+        r = base_hash(&r)
+    }
+
+    r
+}
+
+fn base_hash(s: &Hashed) -> Hashed {
+    let r: String = s.iter().collect();
+    let mut hasher = Md5::new();
+    hasher.update(r);
+
+    as_s(&hasher.finalize())
+}
+
+fn hasher(salt: &str, n: Num, steps: Num) -> Hashed {
+    let s = salt.to_owned() + &(n.to_string());
+    let r = s.chars().collect();
+    find_hash(&r, steps)
 }
 
 fn all_eq<T: Eq>(xs: &[T]) -> bool {
@@ -29,22 +51,27 @@ fn sequences<T: Eq + Clone>(n: usize, xs: &[T]) -> Vec<Vec<T>> {
         .collect()
 }
 
-fn valid(n: Num, salt: &str) -> bool {
-    let h = hasher(salt, n);
+fn valid(n: Num, salt: &str, steps: Num) -> bool {
+    let h = hasher(salt, n, steps);
     if let Some(s) = sequences(3, &h).get(0) {
         let a = s[0];
         let desired = vec![a; 5];
 
-        let posses = ((n + 1)..(n + 1 + 1000)).map(|k| hasher(salt, k));
-        posses.flat_map(|p| sequences(5, &p)).any(|r| r == desired)
+        let posses = ((n + 1)..(n + 1 + 1000))
+            .into_par_iter()
+            .map(|k| hasher(salt, k, steps));
+        posses
+            .into_par_iter()
+            .flat_map(|p| sequences(5, &p))
+            .any(|r| r == desired)
     } else {
         false
     }
 }
 
 pub fn part1(s: &str) -> Num {
-    (1..).filter(|&n| valid(n, s)).nth(63).unwrap()
+    (0..).filter(|&n| valid(n, s, 1)).nth(63).unwrap()
 }
 pub fn part2(s: &str) -> Num {
-    todo!();
+    (0..).filter(|&n| valid(n, s, 2017)).nth(63).unwrap()
 }
