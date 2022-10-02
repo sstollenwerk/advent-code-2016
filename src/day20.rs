fa3f98befa65bb02a468ba7e22f20bdbcf558dfb
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-03 08:24:41 +1300

    Do day20 part1

--- src/day20.rs
+++ src/day20.rs
@@ -1,6 +1,10 @@
+use std::iter;
+
 type Num = u64;
 
 type Group = (Num, Num);
+
+const LIMIT: Num = 4294967296;
 
 fn parse_row(s: &str) -> Group {
     let mut parts = s.split('-').map(|c| c.parse().unwrap());
@@ -27,6 +31,14 @@ fn merge_rows(xs_: &[Group]) -> Vec<Group> {
 fn parse(s: &str) -> Vec<Group> {
     s.lines().map(parse_row).collect()
 }
+
+fn permitted(xs: &[Group]) -> Num {
+    let banned = merge_rows(xs);
+    let last = iter::once((banned.last().unwrap().1, LIMIT));
+    let allowed = banned.windows(2).map(|g| (g[0].1, g[1].0)).chain(last);
+    allowed.map(|(a, b)| b - a).sum()
+}
+
 pub fn part1(s: &str) -> Num {
     let vals = parse(s);
     let res = merge_rows(&vals);
@@ -36,5 +48,5 @@ pub fn part1(s: &str) -> Num {
 
 pub fn part2(s: &str) -> Num {
     let vals = parse(s);
-    todo!();
+    permitted(&vals)
 }
